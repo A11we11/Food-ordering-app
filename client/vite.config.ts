a86f0786ff8cb324ff7fa2c94bbe7f06c78e7c4c
0llwell <ogbonnaallwell1999@gmail.com>
2025-08-28 09:47:31 +0100

    vit config fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from "vitest/config.js";
+/* import { defineConfig } from "vitest/config.js"; */
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
